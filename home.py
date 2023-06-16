c89f85993451a762f652037c9d5b2c2db5bb4064
mewkung <110953766+mewkung@users.noreply.github.com>
2023-06-16 15:14:18 +0700

    .commit

--- home.py
+++ home.py
@@ -25,20 +25,8 @@ with col3:
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
-dt = pd.read_csv("./data/iris.csv")
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
 
-st.write(dt.head(10))
-
-dt1 = dt['petal.length'].sum()
-dt2 = dt['petal.width'].sum()
-dt3 = dt['sepal.length'].sum()
-dt4 = dt['sepal.width'].sum()
-
-dx = [dt1, dt2, dt3, dt4]
-dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
-if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   #st.write(dt.head(10))
-   st.bar_chart(dx2)
-   st.button("ไม่แสดงข้อมูล")
-else:
-    st.write("ไม่แสดงข้อมูล")
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
