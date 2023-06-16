caa19e404eff7b678854d989260b92afa9b37816
mewkung <110953766+mewkung@users.noreply.github.com>
2023-06-16 14:56:57 +0700

    .commit

--- home.py
+++ home.py
@@ -5,12 +5,12 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Versicolor")
-   st.image("./pic/iris1.jpg")
+   st.image("./Pic/iris1.jpg")
 
 with col2:
    st.header("Verginiga")
-   st.image("./pic/iris2.jpg")
+   st.image("./Pic/iris2.jpg")
 
 with col3:
    st.header("Setosa")
-   st.image("./pic/iris3.jpg")
+   st.image("./Pic/iris3.jpg")
