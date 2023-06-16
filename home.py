f3bfa3f6e1e6e879cc9cae1bfe5e07401c82f28d
mewkung <110953766+mewkung@users.noreply.github.com>
2023-06-16 15:27:02 +0700

    .commit

--- home.py
+++ home.py
@@ -1,4 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.header('Treedanai')
 st.image("./Pic/Treedanai.png")
@@ -25,6 +31,33 @@ with col3:
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
+
+dt = pd.read_csv("./data/iris.csv")
+
+st.write(dt.head(10))
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   #st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
 pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
 pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
 
@@ -32,6 +65,6 @@ sp_len = st.number_input("กรุณาเลือกข้อมูล sepal
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
 
 if st.button("ทำนายผล"):
-    st.write("meokp")
+    st.write("ทำนาย")
 else:
     st.write("ไม่ทำนาย")
