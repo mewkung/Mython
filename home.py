a9bbbcc8d1616ee560497d7bbde9404f4102d1f4
mewkung <110953766+mewkung@users.noreply.github.com>
2023-06-16 15:50:22 +0700

    .commit

--- home.py
+++ home.py
@@ -23,12 +23,12 @@ with col3:
    st.header("Setosa")
    st.image("./Pic/iris3.jpg")
 
-   html_8 = """
+   html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
-st.markdown(html_8, unsafe_allow_html=True)
+st.markdown(html_7, unsafe_allow_html=True)
 st.markdown("")
 
 
