2e23a676695a0c88766dfaf4607f79aa6bcfa3da
mewkung <110953766+mewkung@users.noreply.github.com>
2023-06-16 15:12:34 +0700

    .commit

--- home.py
+++ home.py
@@ -24,3 +24,21 @@ with col3:
 """
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
+
+dt = pd.read_csv("./data/iris.csv")
+
+st.write(dt.head(10))
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   #st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
